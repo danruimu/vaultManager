15aa1d83fb06a0a90462621cbc7ee10fce1dae7e
Dani Ruiz <dani.ruiz.89@gmail.com>
2015-09-29 09:18:14 +0200

    refactor

--- walletManager/src/vaultManager/Main.java
+++ walletManager/src/vaultManager/Main.java
@@ -3,7 +3,6 @@ package vaultManager;
 public class Main {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
 	dbDriver db = new dbDriver();
 	db.initDatabase("prueba", "dawan.ddns.net:3306", "test", "test");
     }
